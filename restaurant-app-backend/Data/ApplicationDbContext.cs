1fafe87cc42ea03848a66ba122f41ed69bf37e4c
Igor Czeszejko-Sochacki <mortis3@op.pl>
2021-03-20 15:28:13 +0100

    Initial commit

---
+++ restaurant-app-backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using restaurant_app_backend.Models;
+
+namespace restaurant_app_backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<UserModel>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+            // DbSet<UserModel> Users
+        }
+    }
+}
